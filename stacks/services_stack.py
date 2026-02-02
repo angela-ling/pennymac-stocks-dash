7db4901cffdd0b2767d2d501bc409af39ae7000e
angela-ling <114694052+angela-ling@users.noreply.github.com>
2026-02-01 21:34:59 -0800

    Created stacks

---
+++ stacks/services_stack.py
@@ -0,0 +1,43 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_lambda as _lambda,
+    CfnOutput
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class ServicesStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Define lambda function
+        my_function = _lambda.Function(
+            self, "ServicesHelloFunction",
+            runtime = _lambda.Runtime.NODEJS_22_X,
+            handler = "index.handler",
+            code = _lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify('Hello World - Services Stack'),
+                    };
+                };
+                """
+            ),
+        )
+
+        my_function_url = my_function.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE,
+        )
+
+        CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "PennymacStocksDashQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
